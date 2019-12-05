fe509ca38a1c9ea0914341f03c16d607c8b2e171
Олег Чабан <42222997+Gekata@users.noreply.github.com>
2019-12-05 22:57:57 +0200

    Add files via upload

---
+++ average.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class average {
+
+    public static void main(String[] args) {
+        int array[] = {2, -9, 12, 33, 4, -2, 1234};
+        double sum = 0;
+        for (int x : array) {
+            sum += x;
+        }
+        System.out.print("answer: " + sum / array.length);
+    }
+}
