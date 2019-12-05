5d21b609fd892c85838bdc153e21c66ce2678d64
Олег Чабан <42222997+Gekata@users.noreply.github.com>
2019-12-05 23:21:42 +0200

    Update Average.java

--- Average.java
+++ Average.java
@@ -5,8 +5,8 @@ public class Average {
     public static void main(String[] args) {
         int[] array = {2, -9, 12, 33, 4, -2, 1234};
         double sum = 0;
-        for (int x : array) {
-            sum += x;
+        for (int i : array) {
+            sum += i;
         }
         System.out.print("answer: " + sum / array.length);
     }
