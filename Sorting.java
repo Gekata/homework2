88bdb023749fc27173bafe2b3ae5c01c155fcd79
Олег Чабан <42222997+Gekata@users.noreply.github.com>
2019-12-05 23:18:18 +0200

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class Sorting {
 
     public static void main(String[] args) {
-        Integer arrayOfInts[] = {2, 3, 1, 7, 11};
+        Integer[] arrayOfInts = {2, 3, 1, 7, 11};
         Arrays.sort(arrayOfInts, Collections.reverseOrder());
         System.out.println(Arrays.toString(arrayOfInts));
     }
