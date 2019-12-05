b7d6d3269fd8896a3e559b8b16495ee483185762
Олег Чабан <42222997+Gekata@users.noreply.github.com>
2019-12-05 23:15:30 +0200

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -3,7 +3,7 @@ package com.company;
 public class Sum {
 
     public static void main(String[] args) {
-        int array[] = {2, -9, 12, 33, 4, -2, 1234};
+        int[] array = {2, -9, 12, 33, 4, -2, 1234};
         double sum = 0;
         for (int i : array) {
             if (i > 0)
