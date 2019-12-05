6c2d53f1c59c8c23539a41c1677dc49fa35b18b3
Олег Чабан <42222997+Gekata@users.noreply.github.com>
2019-12-05 23:14:31 +0200

    Update Average.java

--- Average.java
+++ Average.java
@@ -3,7 +3,7 @@ package com.company;
 public class Average {
 
     public static void main(String[] args) {
-        int array[] = {2, -9, 12, 33, 4, -2, 1234};
+        int[] array = {2, -9, 12, 33, 4, -2, 1234};
         double sum = 0;
         for (int x : array) {
             sum += x;
